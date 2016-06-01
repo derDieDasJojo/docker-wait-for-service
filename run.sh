c6424b803533787a852360c7cb89fb732bb2e8fb
Jojo <rueckwerts@web.de>
2016-06-01 08:07:10 +0000

    bla

--- run.sh
+++ run.sh
@@ -5,7 +5,9 @@ PORT=27017
 
 #wait for service to come up
 echo "waiting for db to come up .. "
-while ! ./nc -q 1 $HOST $PORT </dev/null; do sleep 10; done
+#while ! /opt/parse-conf/nc -q 1 $HOST $PORT </dev/null; do sleep 10; done
+while ! echo exit | /opt/parse-conf/nc $HOST $PORT; do sleep 10; done
+
 echo "db reached !"
 
 #start node
