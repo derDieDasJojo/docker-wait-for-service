0cf15ec1267668ca2d31a2c33ad1e0564b308de2
Jojo <rueckwerts@web.de>
2016-06-01 08:44:24 +0000

    blalba

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM busybox:latest
 
 ADD waitForService.sh /opt/wait/waitForService.sh
-ADD waitTime.sh /opt/wait/waitTime.sh
 RUN cp /bin/nc /opt/wait/nc
 VOLUME /opt/wait
 CMD /opt/wait/waitForService.sh
--- waitForService.sh
+++ waitForService.sh
@@ -2,11 +2,17 @@
 
 #WAIT_HOST=mongo-cluster #defined by docker-compose-file
 #WAIT_PORT=27017	 #defined by docker-compose-file
+#WAIT_TIME=5		 #defined by docker-compose-file
 
 #wait for service to come up
 echo "waiting for $WAIT_HOST:$WAIT_PORT to come up .. "
 while ! echo exit | /opt/wait/nc $WAIT_HOST $WAIT_PORT; do sleep 10; done
 echo "Service $WAIT_HOST:$WAIT_PORT reached !"
+
+
+#wait for some seconds 
+echo "waiting $WAIT_TIME Seconds .. "
+sleep $WAIT_TIME
 
 #start node
 echo "starting server"
