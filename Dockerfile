4ed33d7087b003b8502f36211216ce42db4a5c4c
Jojo <rueckwerts@bitbucket.org>
2016-06-01 09:57:27 +0200

    local nc

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM busybox:latest
 
+RUN cp /bin/nc /opt/parse-conf/nc
 ADD run.sh /opt/parse-conf/run.sh
 VOLUME /opt/parse-conf
 CMD /opt/parse-conf/run.sh
--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ PORT=27017
 
 #wait for service to come up
 echo "waiting for db to come up .. "
-while ! nc -q 1 $HOST $PORT </dev/null; do sleep 10; done
+while ! ./nc -q 1 $HOST $PORT </dev/null; do sleep 10; done
 echo "db reached !"
 
 #start node
